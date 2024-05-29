96ca1b7e7433e6fad6c64a0a2c129f6b8166fdf7
Lucas1230550 <1230550@isep.ipp.pt>
2024-05-29 17:08:41 +0100

    classe user no user.h do model

--- TastyApp/headers/model/User.h
+++ TastyApp/headers/model/User.h
@@ -1,8 +1,25 @@
-//
-// Created by lucca on 26/05/2024.
-//
-
 #ifndef TASTYAPP_USER_H
 #define TASTYAPP_USER_H
+
+#include <string>
+#include <vector>
+#include "Recipe.h"
+#include "Profile.h
+
+using namespace std;
+
+class User {
+public:
+    User (const string& name);
+    void setName(const string& name);
+    string getName() const;
+    void addRecipe(const Recipe& recipe);
+    vector<Recipe>getRecipes() const;
+    Profile& getProfile();
+
+private:
+    string name;
+
+};
 
 #endif //TASTYAPP_USER_H
