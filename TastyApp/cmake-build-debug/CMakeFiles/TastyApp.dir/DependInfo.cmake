3a1a7252354ac34854bb5dd40af364eb21202100
anamartasilvaa <1230859@isep.ipp.pt>
2024-05-29 17:10:01 +0100

    recipescontroller

---
+++ TastyApp/cmake-build-debug/CMakeFiles/TastyApp.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/anama/Documents/GitHub/FSOFT2024_1DA_5/TastyApp/main.cpp" "CMakeFiles/TastyApp.dir/main.cpp.obj" "gcc" "CMakeFiles/TastyApp.dir/main.cpp.obj.d"
+  "C:/Users/anama/Documents/GitHub/FSOFT2024_1DA_5/TastyApp/sources/controllers/GroupController.cpp" "CMakeFiles/TastyApp.dir/sources/controllers/GroupController.cpp.obj" "gcc" "CMakeFiles/TastyApp.dir/sources/controllers/GroupController.cpp.obj.d"
+  "C:/Users/anama/Documents/GitHub/FSOFT2024_1DA_5/TastyApp/sources/controllers/ProfileController.cpp" "CMakeFiles/TastyApp.dir/sources/controllers/ProfileController.cpp.obj" "gcc" "CMakeFiles/TastyApp.dir/sources/controllers/ProfileController.cpp.obj.d"
+  "C:/Users/anama/Documents/GitHub/FSOFT2024_1DA_5/TastyApp/sources/controllers/RatingController.cpp" "CMakeFiles/TastyApp.dir/sources/controllers/RatingController.cpp.obj" "gcc" "CMakeFiles/TastyApp.dir/sources/controllers/RatingController.cpp.obj.d"
+  "C:/Users/anama/Documents/GitHub/FSOFT2024_1DA_5/TastyApp/sources/controllers/RecipesController.cpp" "CMakeFiles/TastyApp.dir/sources/controllers/RecipesController.cpp.obj" "gcc" "CMakeFiles/TastyApp.dir/sources/controllers/RecipesController.cpp.obj.d"
+  "C:/Users/anama/Documents/GitHub/FSOFT2024_1DA_5/TastyApp/sources/controllers/UserController.cpp" "CMakeFiles/TastyApp.dir/sources/controllers/UserController.cpp.obj" "gcc" "CMakeFiles/TastyApp.dir/sources/controllers/UserController.cpp.obj.d"
+  "C:/Users/anama/Documents/GitHub/FSOFT2024_1DA_5/TastyApp/sources/controllers/UtilsController.cpp" "CMakeFiles/TastyApp.dir/sources/controllers/UtilsController.cpp.obj" "gcc" "CMakeFiles/TastyApp.dir/sources/controllers/UtilsController.cpp.obj.d"
+  "C:/Users/anama/Documents/GitHub/FSOFT2024_1DA_5/TastyApp/sources/model/Group.cpp" "CMakeFiles/TastyApp.dir/sources/model/Group.cpp.obj" "gcc" "CMakeFiles/TastyApp.dir/sources/model/Group.cpp.obj.d"
+  "C:/Users/anama/Documents/GitHub/FSOFT2024_1DA_5/TastyApp/sources/model/GroupContainer.cpp" "CMakeFiles/TastyApp.dir/sources/model/GroupContainer.cpp.obj" "gcc" "CMakeFiles/TastyApp.dir/sources/model/GroupContainer.cpp.obj.d"
+  "C:/Users/anama/Documents/GitHub/FSOFT2024_1DA_5/TastyApp/sources/model/Profile.cpp" "CMakeFiles/TastyApp.dir/sources/model/Profile.cpp.obj" "gcc" "CMakeFiles/TastyApp.dir/sources/model/Profile.cpp.obj.d"
+  "C:/Users/anama/Documents/GitHub/FSOFT2024_1DA_5/TastyApp/sources/model/ProfileContainer.cpp" "CMakeFiles/TastyApp.dir/sources/model/ProfileContainer.cpp.obj" "gcc" "CMakeFiles/TastyApp.dir/sources/model/ProfileContainer.cpp.obj.d"
+  "C:/Users/anama/Documents/GitHub/FSOFT2024_1DA_5/TastyApp/sources/model/Rating.cpp" "CMakeFiles/TastyApp.dir/sources/model/Rating.cpp.obj" "gcc" "CMakeFiles/TastyApp.dir/sources/model/Rating.cpp.obj.d"
+  "C:/Users/anama/Documents/GitHub/FSOFT2024_1DA_5/TastyApp/sources/model/RatingContainer.cpp" "CMakeFiles/TastyApp.dir/sources/model/RatingContainer.cpp.obj" "gcc" "CMakeFiles/TastyApp.dir/sources/model/RatingContainer.cpp.obj.d"
+  "C:/Users/anama/Documents/GitHub/FSOFT2024_1DA_5/TastyApp/sources/model/RecipeContainer.cpp" "CMakeFiles/TastyApp.dir/sources/model/RecipeContainer.cpp.obj" "gcc" "CMakeFiles/TastyApp.dir/sources/model/RecipeContainer.cpp.obj.d"
+  "C:/Users/anama/Documents/GitHub/FSOFT2024_1DA_5/TastyApp/sources/model/RecipePage.cpp" "CMakeFiles/TastyApp.dir/sources/model/RecipePage.cpp.obj" "gcc" "CMakeFiles/TastyApp.dir/sources/model/RecipePage.cpp.obj.d"
+  "C:/Users/anama/Documents/GitHub/FSOFT2024_1DA_5/TastyApp/sources/model/Recipes.cpp" "CMakeFiles/TastyApp.dir/sources/model/Recipes.cpp.obj" "gcc" "CMakeFiles/TastyApp.dir/sources/model/Recipes.cpp.obj.d"
+  "C:/Users/anama/Documents/GitHub/FSOFT2024_1DA_5/TastyApp/sources/model/User.cpp" "CMakeFiles/TastyApp.dir/sources/model/User.cpp.obj" "gcc" "CMakeFiles/TastyApp.dir/sources/model/User.cpp.obj.d"
+  "C:/Users/anama/Documents/GitHub/FSOFT2024_1DA_5/TastyApp/sources/model/UserContainer.cpp" "CMakeFiles/TastyApp.dir/sources/model/UserContainer.cpp.obj" "gcc" "CMakeFiles/TastyApp.dir/sources/model/UserContainer.cpp.obj.d"
+  "C:/Users/anama/Documents/GitHub/FSOFT2024_1DA_5/TastyApp/sources/model/Utils.cpp" "CMakeFiles/TastyApp.dir/sources/model/Utils.cpp.obj" "gcc" "CMakeFiles/TastyApp.dir/sources/model/Utils.cpp.obj.d"
+  "C:/Users/anama/Documents/GitHub/FSOFT2024_1DA_5/TastyApp/sources/views/GroupView.cpp" "CMakeFiles/TastyApp.dir/sources/views/GroupView.cpp.obj" "gcc" "CMakeFiles/TastyApp.dir/sources/views/GroupView.cpp.obj.d"
+  "C:/Users/anama/Documents/GitHub/FSOFT2024_1DA_5/TastyApp/sources/views/ProfileView.cpp" "CMakeFiles/TastyApp.dir/sources/views/ProfileView.cpp.obj" "gcc" "CMakeFiles/TastyApp.dir/sources/views/ProfileView.cpp.obj.d"
+  "C:/Users/anama/Documents/GitHub/FSOFT2024_1DA_5/TastyApp/sources/views/RatingView.cpp" "CMakeFiles/TastyApp.dir/sources/views/RatingView.cpp.obj" "gcc" "CMakeFiles/TastyApp.dir/sources/views/RatingView.cpp.obj.d"
+  "C:/Users/anama/Documents/GitHub/FSOFT2024_1DA_5/TastyApp/sources/views/RecipesView.cpp" "CMakeFiles/TastyApp.dir/sources/views/RecipesView.cpp.obj" "gcc" "CMakeFiles/TastyApp.dir/sources/views/RecipesView.cpp.obj.d"
+  "C:/Users/anama/Documents/GitHub/FSOFT2024_1DA_5/TastyApp/sources/views/UserView.cpp" "CMakeFiles/TastyApp.dir/sources/views/UserView.cpp.obj" "gcc" "CMakeFiles/TastyApp.dir/sources/views/UserView.cpp.obj.d"
+  "C:/Users/anama/Documents/GitHub/FSOFT2024_1DA_5/TastyApp/sources/views/UtilsView.cpp" "CMakeFiles/TastyApp.dir/sources/views/UtilsView.cpp.obj" "gcc" "CMakeFiles/TastyApp.dir/sources/views/UtilsView.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- TastyApp/headers/controllers/RecipesController.h
+++ TastyApp/headers/controllers/RecipesController.h
@@ -5,4 +5,15 @@
 #ifndef TASTYAPP_RECIPESCONTROLLER_H
 #define TASTYAPP_RECIPESCONTROLLER_H
 
+#include "RecipeController.h"
+
+void RecipeController::addRecipe(const Recipe &recipe) {
+    recipes.push_back(recipe);
+}
+
+std::vector<Recipe> RecipeController::getRecipes() const {
+    return recipes;
+}
+
+
 #endif //TASTYAPP_RECIPESCONTROLLER_H
