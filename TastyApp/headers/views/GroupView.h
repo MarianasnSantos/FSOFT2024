4792eb8d0c6db88295f11e1f5203396ee1d79b6e
Lucas1230550 <1230550@isep.ipp.pt>
2024-06-09 21:15:06 +0100

    Criacao do GroupView

--- TastyApp/headers/views/GroupView.h
+++ TastyApp/headers/views/GroupView.h
@@ -1,8 +1,24 @@
-//
-// Created by lucca on 26/05/2024.
-//
 
 #ifndef TASTYAPP_GROUPVIEW_H
 #define TASTYAPP_GROUPVIEW_H
+
+#include <string>
+#include "Group.h"
+#include "User.h"
+
+using namespace std;
+
+class GroupView {
+public:
+    GroupView();
+
+    int menuGroup();
+    string getGroupName();
+    User getUser();
+    void printGroupCreated(const string& groupName);
+    void printJoinedGroup(const string& groupName);
+    void printGroupRemoved(const string& groupName);
+    void printGroupNotFound();
+};
 
 #endif //TASTYAPP_GROUPVIEW_H
--- TastyApp/sources/views/GroupView.cpp
+++ TastyApp/sources/views/GroupView.cpp
@@ -1,3 +1,49 @@
 #include "GroupView.h"
+#include "Utils.h"
+#include <iostream>
 
 using namespace std;
+
+GroupView::GroupView() {}
+
+int GroupView::menuGroup() {
+    int op = -1;
+
+    do {
+        cout << "\n\n========== Menu Group ==========\n";
+        cout << "1 - Create Group\n";
+        cout << "2 - Join Group\n";
+        cout << "3 - Remove Group\n";
+        cout << "\n0 - EXIT\n";
+
+        op = Utils::getNumber("Option");
+    } while (op < 0 || op > 3);
+
+    return op;
+}
+
+string GroupView::getGroupName() {
+    return Utils::getString("Enter the group name: ");
+}
+
+User GroupView::getUser() {
+    string username = Utils::getString("Enter the user name: ");
+    // Assuming there is a constructor that takes a username
+    return User(username);
+}
+
+void GroupView::printGroupCreated(const string& groupName) {
+    cout << "Group '" << groupName << "' created successfully!\n";
+}
+
+void GroupView::printJoinedGroup(const string& groupName) {
+    cout << "Joined group '" << groupName << "' successfully!\n";
+}
+
+void GroupView::printGroupRemoved(const string& groupName) {
+    cout << "Group '" << groupName << "' removed successfully!\n";
+}
+
+void GroupView::printGroupNotFound() {
+    cout << "Group not found!\n";
+}
